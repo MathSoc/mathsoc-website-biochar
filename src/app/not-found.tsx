d7f279b860d24e601e31b101769a5ceefba4c9ae
some-molasses <river.stanley173@gmail.com>
2025-08-09 23:29:18 -0700

    Fix not found again, banners

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,13 @@
-import { Button } from "./components/button/button";
 import "./components/page/page.scss";
 
+// somehow, importing any react component into this file
+// breaks all subsequent imports of that component across the site.
+// i cannot fathom why. beware.
 export default function NotFound() {
   return (
     <div className="page">
       <div className="page-inner">
-        <h2>Error 404: Not Found</h2>
-
-        <Button href="/" variant="pink">
-          Home
-        </Button>
+        <h2>Page not found :(</h2>
       </div>
     </div>
   );
