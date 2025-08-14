ac725d169a1ddf6cb75adf993ea8532ebfe3e097
River Stanley <rstanley@splunk.com>
2025-08-14 12:42:52 -0700

    Add sign out

--- src/app/auth-dev/signin/page.tsx
+++ src/app/auth-dev/signin/page.tsx
@@ -1,14 +1,28 @@
 import { Page } from "../../components/page/page-component";
 import { Metadata } from "next";
-import { CustomSignIn } from "./actions";
+import { signIn, signOut } from "../../../../auth";
 
 export const metadata: Metadata = { title: "auth test" };
 
 export default async function ProtectedPage() {
   return (
     <Page id="test-page-2">
-      <form action={CustomSignIn}>
+      <form
+        action={() => {
+          "use server";
+          signIn();
+        }}
+      >
         <button type="submit">Signin with Google</button>
+      </form>
+
+      <form
+        action={() => {
+          "use server";
+          signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
       </form>
     </Page>
   );
