a6b8ab4636b78b5a16860bf50cb13d1400a696aa
some-molasses <river.stanley173@gmail.com>
2025-08-17 21:37:03 -0700

    Fill out uploads table

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,13 +3,16 @@ import { Page } from "../components/page/page-component";
 import { Button } from "../components/button/button";
 import { regenerateExamsListAction } from "../util/exam-actions";
 import { ExamsTable } from "../components/tables/exams-table/exams-table";
+import { ExamUploads } from "./components/exam-uploads";
 
 // @todo protect this page
 export default function Home() {
   return (
     <Page id="admin-home">
       <h1>MathSoc Admin</h1>
-      <h2>Exam bank</h2>
+      <h2>Add to exam bank</h2>
+      <ExamUploads />
+      <h2>Manage exam bank</h2>
       <ExamsTable isAdmin />
       <Button action={regenerateExamsListAction} variant="pink">
         regenerate exams list (danger!)
