d90eb16958feb4070aecfe892047b0501c282d93
Samir Sharma <samirraisharma@gmail.com>
2025-08-22 13:05:48 -0400

    Redirects from wordpress links to new links for documents (#3)
    * Redirects from wordpress links to new links for documents
    
    * fixed redirect error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/wp-content/uploads/:path*",
+        destination: "/documents/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
