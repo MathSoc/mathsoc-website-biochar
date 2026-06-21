11b3b51602ed2cab2216dc752bff9e3b8663f1ef
some-molasses <river.stanley173@gmail.com>
2026-06-20 22:15:37 -0400

    Add documentation

--- src/app/api/resources/policies/[type]/route.ts
+++ src/app/api/resources/policies/[type]/route.ts
@@ -2,6 +2,11 @@ import { NextResponse } from "next/server";
 
 const POLICY_TYPES = ["policies", "board-procedures", "bylaws"];
 
+/**
+ * Downloads the most up-to-date version of MathSoc's bylaws from the mathsocDocuments repo,
+ * then displays it to the user. This hides the download; the pdf is not downloaded as a new file
+ * on the client's device.
+ */
 export async function GET(
   _: Request,
   context: RouteContext<"/api/resources/policies/[type]">,
