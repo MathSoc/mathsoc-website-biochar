4fb38c8cceabb890bb1c9981e52b097d6ea798c1
Jiamiao Zeng <angelazeng2021@gmail.com>
2026-09-11 21:33:54 -0400

    config changes & middleware export

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,7 @@
+import { auth as middleware } from "./auth";
+
+export { middleware };
+
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
